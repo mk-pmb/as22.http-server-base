b4f8ab2280bafb7bb8c42e35acf3b3f72d32dcd0
mk-pmb <mk-pmb@users.noreply.github.com>
2026-03-15 23:50:35 +0100

    Read config from environment and make listen address configurable.

--- src/makeServer.mjs
+++ src/makeServer.mjs
@@ -1,15 +1,33 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import envcfgMergeConfigs from 'envcfg-merge-configs-pmb';
 import express from 'express';
+import mustBe from 'typechecks-pmb/must-be.js';
 import nodeHttp from 'node:http';
+import objPop from 'objpop';
 
 
-const EX = async function createServer() {
+import installListenAddrPlumbing from './listenAddrPlumbing.mjs';
+
+
+const EX = async function createServer(customConfig) {
+  const entireConfig = envcfgMergeConfigs({ ifPrefixProp: 'envcfg_prefix' },
+    EX.cliConfigDefaults, customConfig);
+  const popCfg = objPop.d(entireConfig, { mustBe }).mustBe;
+  popCfg('str | eeq:false', 'envcfg_prefix');
+  console.debug('Server config:', entireConfig);
 
   const webSrv = nodeHttp.createServer();
   const srv = {
     getLowLevelWebServer() { return webSrv; },
-    listen() { webSrv.listen(); },
+    logMsg: console.log.bind(console),
+    popCfg,
+
+    initialConfigDone() {
+      popCfg.expectEmpty('Unsupported server config option(s)');
+      srv.popCfg = EX.denyLateConfigRead;
+    },
+
   };
 
   const app = express();
@@ -22,9 +40,38 @@ const EX = async function createServer() {
     console.debug('App cleanup:', args);
   });
 
+  await installListenAddrPlumbing(srv);
+
   webSrv.on('request', app);
   return srv;
 };
+
+
+const guessDockerized = (process.getuid() === 0);
+
+
+Object.assign(EX, {
+
+  cliConfigDefaults: {
+
+    envcfg_prefix: 'anno_',
+
+    listen_addr: (guessDockerized ? '0.0.0.0' : '127.0.0.1'),
+    listen_port: 8080,
+    notify_server_listening: '',
+    public_baseurl: '',
+
+  },
+
+
+  denyLateConfigRead(expectedType, slot) {
+    const err = new Error('Late attempt to read server config');
+    Object.assign(err, { expectedType, slot });
+    throw err;
+  },
+
+
+});
 
 
 export default EX;
